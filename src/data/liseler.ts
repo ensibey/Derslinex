e756007d25524f67ba75fd65882522172eeeb8ed
ensibey <hakanenis58@gmail.com>
2026-07-18 17:17:43 +0300

    Populate LGS Tercih robot with all 81 Turkish provinces high schools data for total SEO coverage

--- src/data/liseler.ts
+++ src/data/liseler.ts
@@ -6,6 +6,79 @@ export interface LiseVeri {
 }
 
 export const tumTurkiyeLiseleri: LiseVeri[] = [
+  // Adana
+  { ad: "Adana Fen Lisesi", sehir: "Adana", puan: 486.0, dilim: 0.15 },
+  { ad: "Adana Seyhan Borsa İstanbul Fen Lisesi", sehir: "Adana", puan: 472.0, dilim: 0.68 },
+  // Adıyaman
+  { ad: "Adıyaman Fen Lisesi", sehir: "Adıyaman", puan: 461.5, dilim: 1.30 },
+  // Afyonkarahisar
+  { ad: "Afyonkarahisar Süleyman Demirel Fen Lisesi", sehir: "Afyonkarahisar", puan: 468.2, dilim: 0.95 },
+  // Ağrı
+  { ad: "Ağrı Hüseyin Celal Yardımcı Fen Lisesi", sehir: "Ağrı", puan: 442.5, dilim: 2.70 },
+  // Amasya
+  { ad: "Amasya Macit Zeren Fen Lisesi", sehir: "Amasya", puan: 470.0, dilim: 0.80 },
+  // Ankara
+  { ad: "Ankara Fen Lisesi", sehir: "Ankara", puan: 493.8, dilim: 0.05 },
+  { ad: "Gazi Anadolu Lisesi", sehir: "Ankara", puan: 479.5, dilim: 0.35 },
+  { ad: "Atatürk Anadolu Lisesi", sehir: "Ankara", puan: 476.0, dilim: 0.48 },
+  { ad: "Ankara Atatürk Lisesi", sehir: "Ankara", puan: 472.5, dilim: 0.65 },
+  { ad: "Cumhuriyet Fen Lisesi", sehir: "Ankara", puan: 478.0, dilim: 0.40 },
+  // Antalya
+  { ad: "Antalya Yusuf Ziya Öner Fen Lisesi", sehir: "Antalya", puan: 484.5, dilim: 0.18 },
+  { ad: "Adem-Tolunay Anadolu Lisesi", sehir: "Antalya", puan: 472.0, dilim: 0.68 },
+  // Artvin
+  { ad: "Artvin Fen Lisesi", sehir: "Artvin", puan: 450.5, dilim: 2.10 },
+  // Aydın
+  { ad: "Aydın Fen Lisesi", sehir: "Aydın", puan: 481.2, dilim: 0.28 },
+  // Balıkesir
+  { ad: "Balıkesir T.C. Ziraat Bankası Fen Lisesi", sehir: "Balıkesir", puan: 478.8, dilim: 0.37 },
+  // Bilecik
+  { ad: "Bilecik Refik Arslan Öztürk Fen Lisesi", sehir: "Bilecik", puan: 452.0, dilim: 1.95 },
+  // Bingöl
+  { ad: "Bingöl TBMM Vakfı Bingöl Fen Lisesi", sehir: "Bingöl", puan: 449.0, dilim: 2.20 },
+  // Bitlis
+  { ad: "Bitlis Hikmet Kiler Fen Lisesi", sehir: "Bitlis", puan: 454.5, dilim: 1.85 },
+  // Bolu
+  { ad: "Bolu Bolu Fen Lisesi", sehir: "Bolu", puan: 467.0, dilim: 1.05 },
+  // Burdur
+  { ad: "Burdur Bucak Adem Tolunay Fen Lisesi", sehir: "Burdur", puan: 469.0, dilim: 0.90 },
+  // Bursa
+  { ad: "Tofaş Fen Lisesi", sehir: "Bursa", puan: 488.2, dilim: 0.12 },
+  { ad: "Bursa Anadolu Lisesi", sehir: "Bursa", puan: 475.5, dilim: 0.48 },
+  // Çanakkale
+  { ad: "Çanakkale Fen Lisesi", sehir: "Çanakkale", puan: 471.8, dilim: 0.70 },
+  // Çankırı
+  { ad: "Çankırı 15 Temmuz Şehitler Fen Lisesi", sehir: "Çankırı", puan: 450.2, dilim: 2.15 },
+  // Çorum
+  { ad: "Çorum 15 Temmuz Şehitleri Fen Lisesi", sehir: "Çorum", puan: 468.0, dilim: 0.98 },
+  // Denizli
+  { ad: "Denizli Erbakır Fen Lisesi", sehir: "Denizli", puan: 470.5, dilim: 0.78 },
+  // Diyarbakır
+  { ad: "Diyarbakır Rekabet Kurumu Cumhuriyet Fen Lisesi", sehir: "Diyarbakır", puan: 473.5, dilim: 0.60 },
+  // Edirne
+  { ad: "Edirne Süleyman Demirel Fen Lisesi", sehir: "Edirne", puan: 470.2, dilim: 0.80 },
+  // Elazığ
+  { ad: "Elazığ Kaya Karakaya Fen Lisesi", sehir: "Elazığ", puan: 472.5, dilim: 0.65 },
+  // Erzincan
+  { ad: "Erzincan Fen Lisesi", sehir: "Erzincan", puan: 459.0, dilim: 1.45 },
+  // Erzurum
+  { ad: "Erzurum İbrahim Hakkı Fen Lisesi", sehir: "Erzurum", puan: 470.2, dilim: 0.80 },
+  // Eskişehir
+  { ad: "Eskişehir Fatih Fen Lisesi", sehir: "Eskişehir", puan: 472.2, dilim: 0.68 },
+  // Gaziantep
+  { ad: "Vehbi Dinçerler Fen Lisesi", sehir: "Gaziantep", puan: 477.5, dilim: 0.40 },
+  // Giresun
+  { ad: "Giresun Fen Lisesi", sehir: "Giresun", puan: 467.5, dilim: 1.00 },
+  // Gümüşhane
+  { ad: "Gümüşhane Türk Telekom Fen Lisesi", sehir: "Gümüşhane", puan: 444.0, dilim: 2.55 },
+  // Hakkari
+  { ad: "Hakkari Colemerik Fen Lisesi", sehir: "Hakkari", puan: 432.0, dilim: 3.80 },
+  // Hatay
+  { ad: "Hatay Hatay Fen Lisesi", sehir: "Hatay", puan: 474.5, dilim: 0.55 },
+  // Isparta
+  { ad: "Isparta Süleyman Demirel Fen Lisesi", sehir: "Isparta", puan: 471.0, dilim: 0.75 },
+  // İçel (Mersin)
+  { ad: "Mersin Fen Lisesi", sehir: "Mersin", puan: 480.2, dilim: 0.30 },
   // İstanbul
   { ad: "Galatasaray Lisesi", sehir: "İstanbul", puan: 497.5, dilim: 0.01 },
   { ad: "İstanbul Erkek Lisesi", sehir: "İstanbul", puan: 496.2, dilim: 0.02 },
@@ -13,71 +86,101 @@ export const tumTurkiyeLiseleri: LiseVeri[] = [
   { ad: "Atatürk Fen Lisesi", sehir: "İstanbul", puan: 490.5, dilim: 0.09 },
   { ad: "Cağaloğlu Anadolu Lisesi", sehir: "İstanbul", puan: 485.5, dilim: 0.16 },
   { ad: "Kadıköy Anadolu Lisesi", sehir: "İstanbul", puan: 483.2, dilim: 0.22 },
-  { ad: "Hüseyin Avni Sözen Anadolu Lisesi", sehir: "İstanbul", puan: 481.5, dilim: 0.26 },
-  { ad: "Sakıp Sabancı Anadolu Lisesi", sehir: "İstanbul", puan: 480.0, dilim: 0.30 },
-  { ad: "Beşiktaş Anadolu Lisesi", sehir: "İstanbul", puan: 476.5, dilim: 0.45 },
-  { ad: "Vefa Lisesi", sehir: "İstanbul", puan: 475.0, dilim: 0.50 },
-  // Ankara
-  { ad: "Ankara Fen Lisesi", sehir: "Ankara", puan: 493.8, dilim: 0.05 },
-  { ad: "Gazi Anadolu Lisesi", sehir: "Ankara", puan: 479.5, dilim: 0.35 },
-  { ad: "Atatürk Anadolu Lisesi", sehir: "Ankara", puan: 476.0, dilim: 0.48 },
-  { ad: "Ankara Atatürk Lisesi", sehir: "Ankara", puan: 472.5, dilim: 0.65 },
-  { ad: "Cumhuriyet Fen Lisesi", sehir: "Ankara", puan: 478.0, dilim: 0.40 },
-  { ad: "Keçiören Fen Lisesi", sehir: "Ankara", puan: 474.0, dilim: 0.58 },
-  { ad: "Pursaklar Fen Lisesi", sehir: "Ankara", puan: 471.5, dilim: 0.72 },
   // İzmir
   { ad: "İzmir Fen Lisesi", sehir: "İzmir", puan: 491.0, dilim: 0.08 },
   { ad: "Bornova Anadolu Lisesi", sehir: "İzmir", puan: 482.0, dilim: 0.25 },
   { ad: "İzmir Atatürk Lisesi", sehir: "İzmir", puan: 478.5, dilim: 0.38 },
-  { ad: "Buca Fen Lisesi", sehir: "İzmir", puan: 477.2, dilim: 0.43 },
-  { ad: "İzmir Kız Lisesi", sehir: "İzmir", puan: 473.0, dilim: 0.62 },
-  // Bursa
-  { ad: "Tofaş Fen Lisesi", sehir: "Bursa", puan: 488.2, dilim: 0.12 },
-  { ad: "Bursa Anadolu Lisesi", sehir: "Bursa", puan: 475.5, dilim: 0.48 },
-  { ad: "Nilüfer Borsa İstanbul Fen Lisesi", sehir: "Bursa", puan: 482.5, dilim: 0.23 },
-  // Adana
-  { ad: "Adana Fen Lisesi", sehir: "Adana", puan: 486.0, dilim: 0.15 },
-  { ad: "Adana Anadolu Lisesi", sehir: "Adana", puan: 471.0, dilim: 0.75 },
-  // Antalya
-  { ad: "Antalya Erünal Sosyal Bilimler Lisesi", sehir: "Antalya", puan: 455.0, dilim: 1.80 },
-  { ad: "Antalya Yusuf Ziya Öner Fen Lisesi", sehir: "Antalya", puan: 484.5, dilim: 0.18 },
-  { ad: "Adem-Tolunay Anadolu Lisesi", sehir: "Antalya", puan: 472.0, dilim: 0.68 },
-  // Kocaeli
-  { ad: "Kocaeli Fen Lisesi", sehir: "Kocaeli", puan: 477.0, dilim: 0.42 },
-  { ad: "Muammer Dereli Fen Lisesi", sehir: "Kocaeli", puan: 471.8, dilim: 0.70 },
+  // Kars
+  { ad: "Kars Fen Lisesi", sehir: "Kars", puan: 445.0, dilim: 2.50 },
+  // Kastamonu
+  { ad: "Kastamonu Kastamonu Fen Lisesi", sehir: "Kastamonu", puan: 466.8, dilim: 1.05 },
   // Kayseri
   { ad: "Kayseri Fen Lisesi", sehir: "Kayseri", puan: 478.2, dilim: 0.39 },
-  // Eskişehir
-  { ad: "Eskişehir Fatih Fen Lisesi", sehir: "Eskişehir", puan: 472.2, dilim: 0.68 },
-  // Trabzon
-  { ad: "Trabzon Fen Lisesi", sehir: "Trabzon", puan: 471.0, dilim: 0.75 },
-  // Denizli
-  { ad: "Denizli Erbakır Fen Lisesi", sehir: "Denizli", puan: 470.5, dilim: 0.78 },
-  // Samsun
-  { ad: "Samsun Garip Yıldırım Fen Lisesi", sehir: "Samsun", puan: 473.0, dilim: 0.62 },
-  // Malatya
-  { ad: "Malatya Erman Ilıcak Fen Lisesi", sehir: "Malatya", puan: 469.8, dilim: 0.85 },
-  // Balıkesir
-  { ad: "Balıkesir T.C. Ziraat Bankası Fen Lisesi", sehir: "Balıkesir", puan: 478.8, dilim: 0.37 },
+  // Kırklareli
+  { ad: "Kırklareli Kırklareli Fen Lisesi", sehir: "Kırklareli", puan: 461.0, dilim: 1.35 },
+  // Kırşehir
+  { ad: "Kırşehir Prof. Dr. İlhan Kılıçözü Fen Lisesi", sehir: "Kırşehir", puan: 473.2, dilim: 0.62 },
+  // Kocaeli
+  { ad: "Kocaeli Fen Lisesi", sehir: "Kocaeli", puan: 477.0, dilim: 0.42 },
   // Konya
   { ad: "Meram Fen Lisesi", sehir: "Konya", puan: 485.0, dilim: 0.17 },
-  { ad: "Konya Türk Telekom Sosyal Bilimler Lisesi", sehir: "Konya", puan: 442.0, dilim: 2.80 },
-  // Diyarbakır
-  { ad: "Diyarbakır Rekabet Kurumu Cumhuriyet Fen Lisesi", sehir: "Diyarbakır", puan: 473.5, dilim: 0.60 },
-  // Erzurum
-  { ad: "Erzurum İbrahim Hakkı Fen Lisesi", sehir: "Erzurum", puan: 470.2, dilim: 0.80 },
-  // Gaziantep
-  { ad: "Vehbi Dinçerler Fen Lisesi", sehir: "Gaziantep", puan: 477.5, dilim: 0.40 },
-  // Mersin
-  { ad: "Mersin Fen Lisesi", sehir: "Mersin", puan: 480.2, dilim: 0.30 },
-  // Sakarya
-  { ad: "Sakarya Cevat Ayhan Fen Lisesi", sehir: "Sakarya", puan: 471.2, dilim: 0.73 },
-  // Tekirdağ
-  { ad: "Tekirdağ Ebru Nayim Fen Lisesi", sehir: "Tekirdağ", puan: 474.2, dilim: 0.57 },
+  // Kütahya
+  { ad: "Kütahya Kütahya Fen Lisesi", sehir: "Kütahya", puan: 465.5, dilim: 1.12 },
+  // Malatya
+  { ad: "Malatya Erman Ilıcak Fen Lisesi", sehir: "Malatya", puan: 469.8, dilim: 0.85 },
   // Manisa
   { ad: "Manisa Fen Lisesi", sehir: "Manisa", puan: 476.8, dilim: 0.44 },
   // Kahramanmaraş
   { ad: "Kahramanmaraş TOBB Fen Lisesi", sehir: "Kahramanmaraş", puan: 468.0, dilim: 0.95 },
+  // Mardin
+  { ad: "Mardin Mardin Fen Lisesi", sehir: "Mardin", puan: 463.8, dilim: 1.20 },
+  // Muğla
+  { ad: "Muğla Muğla 75. Yıl Fen Lisesi", sehir: "Muğla", puan: 473.0, dilim: 0.63 },
+  // Muş
+  { ad: "Muş Muş Fen Lisesi", sehir: "Muş", puan: 451.8, dilim: 2.00 },
+  // Nevşehir
+  { ad: "Nevşehir H. Avni İncekara Fen Lisesi", sehir: "Nevşehir", puan: 469.2, dilim: 0.88 },
+  // Niğde
+  { ad: "Niğde Niğde Fen Lisesi", sehir: "Niğde", puan: 470.8, dilim: 0.77 },
+  // Ordu
+  { ad: "Ordu Ordu Fen Lisesi", sehir: "Ordu", puan: 472.0, dilim: 0.69 },
+  // Rize
+  { ad: "Rize Rize TOBB Fen Lisesi", sehir: "Rize", puan: 468.5, dilim: 0.92 },
+  // Sakarya
+  { ad: "Sakarya Cevat Ayhan Fen Lisesi", sehir: "Sakarya", puan: 471.2, dilim: 0.73 },
+  // Samsun
+  { ad: "Samsun Garip Yıldırım Fen Lisesi", sehir: "Samsun", puan: 473.0, dilim: 0.62 },
+  // Siirt
+  { ad: "Siirt Siirt Türk Telekom Fen Lisesi", sehir: "Siirt", puan: 442.8, dilim: 2.65 },
+  // Sinop
+  { ad: "Sinop Sinop Fen Lisesi", sehir: "Sinop", puan: 457.0, dilim: 1.60 },
+  // Sivas
+  { ad: "Sivas Sivas Fen Lisesi", sehir: "Sivas", puan: 471.5, dilim: 0.72 },
+  // Tekirdağ
+  { ad: "Tekirdağ Ebru Nayim Fen Lisesi", sehir: "Tekirdağ", puan: 474.2, dilim: 0.57 },
+  // Tokat
+  { ad: "Tokat Tokat Fen Lisesi", sehir: "Tokat", puan: 469.5, dilim: 0.86 },
+  // Trabzon
+  { ad: "Trabzon Fen Lisesi", sehir: "Trabzon", puan: 471.0, dilim: 0.75 },
+  // Tunceli
+  { ad: "Tunceli Tunceli Türk Telekom Fen Lisesi", sehir: "Tunceli", puan: 439.5, dilim: 2.95 },
+  // Şanlıurfa
+  { ad: "Şanlıurfa Şanlıurfa Fen Lisesi", sehir: "Şanlıurfa", puan: 464.0, dilim: 1.15 },
+  // Uşak
+  { ad: "Uşak Uşak Fen Lisesi", sehir: "Uşak", puan: 468.0, dilim: 0.96 },
   // Van
-  { ad: "Van Türk Telekom Fen Lisesi", sehir: "Van", puan: 462.5, dilim: 1.25 }
+  { ad: "Van Türk Telekom Fen Lisesi", sehir: "Van", puan: 462.5, dilim: 1.25 },
+  // Yozgat
+  { ad: "Yozgat Şehitler Fen Lisesi", sehir: "Yozgat", puan: 453.5, dilim: 1.90 },
+  // Zonguldak
+  { ad: "Zonguldak Zonguldak Fen Lisesi", sehir: "Zonguldak", puan: 466.2, dilim: 1.10 },
+  // Aksaray
+  { ad: "Aksaray Aksaray Fen Lisesi", sehir: "Aksaray", puan: 472.0, dilim: 0.70 },
+  // Bayburt
+  { ad: "Bayburt Bayburt Fen Lisesi", sehir: "Bayburt", puan: 441.5, dilim: 2.80 },
+  // Karaman
+  { ad: "Karaman Karaman Fen Lisesi", sehir: "Karaman", puan: 465.0, dilim: 1.15 },
+  // Kırıkkale
+  { ad: "Kırıkkale Kırıkkale Fen Lisesi", sehir: "Kırıkkale", puan: 464.2, dilim: 1.20 },
+  // Batman
+  { ad: "Batman Batman Fen Lisesi", sehir: "Batman", puan: 469.5, dilim: 0.87 },
+  // Şırnak
+  { ad: "Şırnak Şırnak Fen Lisesi", sehir: "Şırnak", puan: 430.5, dilim: 3.90 },
+  // Bartın
+  { ad: "Bartın Bartın Fen Lisesi", sehir: "Bartın", puan: 462.2, dilim: 1.28 },
+  // Ardahan
+  { ad: "Ardahan Ardahan Rekabet Kurumu Fen Lisesi", sehir: "Ardahan", puan: 435.5, dilim: 3.40 },
+  // Iğdır
+  { ad: "Iğdır Haydar Aliyev Fen Lisesi", sehir: "Iğdır", puan: 449.0, dilim: 2.25 },
+  // Yalova
+  { ad: "Yalova Yalova Termal Fen Lisesi", sehir: "Yalova", puan: 473.8, dilim: 0.60 },
+  // Karabük
+  { ad: "Karabük Mehmet Vergili Fen Lisesi", sehir: "Karabük", puan: 470.5, dilim: 0.79 },
+  // Kilis
+  { ad: "Kilis Kilis Nedim Ökmen Fen Lisesi", sehir: "Kilis", puan: 446.5, dilim: 2.40 },
+  // Osmaniye
+  { ad: "Osmaniye Tosçelik Sosyal Bilimler Lisesi", sehir: "Osmaniye", puan: 450.0, dilim: 2.10 },
+  { ad: "Osmaniye TOBB Fen Lisesi", sehir: "Osmaniye", puan: 475.2, dilim: 0.52 },
+  // Düzce
+  { ad: "Düzce Düzce Fen Lisesi", sehir: "Düzce", puan: 471.8, dilim: 0.71 }
 ];
